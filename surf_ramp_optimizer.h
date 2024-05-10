8346b3e66a6d334a4adac4266a5cbf63e5502b9b
dark room danny <115037920+followingthefasciaplane@users.noreply.github.com>
2024-05-11 03:19:36 +1200

    Update surf_ramp_optimizer.h

--- surf_ramp_optimizer.h
+++ surf_ramp_optimizer.h
@@ -6,119 +6,68 @@
 #include <memory>
 #include <optional>
 
-// Forward declarations
+class CBaseEntity;
 class CBrachistochroneOptimizer;
 class PlayerMovementTracking;
 class BVHNode;
 class Matrix;
 class Vector;
 class Ramp;
-class CBaseEntity;
+
 namespace CollisionDetection;
 namespace NumericalMethods;
 namespace MathUtils;
 namespace Optimization;
 namespace Pathfinding;
 
-// Global constants
 constexpr float MAX_PLAYER_DIST_TO_PATH = 50.0f;
 constexpr float GRADIENT_TOLERANCE = 1e-6f;
 constexpr int MAX_OPTIMIZATION_ITERATIONS = 100;
 
-// Vector class
-class Vector
+class SurfRampOptimizer : public SDKExtension
 {
 public:
-    float x, y, z;
-    float v, w, u;
+    virtual bool SDK_OnLoad(char* error, size_t maxlength, bool late);
+    virtual void SDK_OnUnload();
+    virtual void SDK_OnAllLoaded();
+    virtual bool QueryRunning(char* error, size_t maxlength);
 
-    Vector();
-    Vector(float _x, float _y, float _z);
-    Vector(float _x, float _y, float _z, float _v, float _w, float _u);
-
-    float LengthSqr() const;
-    float Length() const;
-    Vector Normalized() const;
-    Vector Cross(const Vector& other) const;
-    float Dot(const Vector& other) const;
-
-    Vector operator+(const Vector& other) const;
-    Vector operator-(const Vector& other) const;
-    Vector operator*(float scalar) const;
-    Vector operator/(float scalar) const;
-    bool operator==(const Vector& other) const;
-    bool operator!=(const Vector& other) const;
-};
-
-// Ramp struct
-struct Ramp
-{
-    Vector startPoint;
-    Vector endPoint;
-    Vector normal;
-    float length;
-    float width;
-    Vector minExtents;
-    Vector maxExtents;
-
-    bool operator==(const Ramp& other) const;
-};
-
-// Matrix class
-class Matrix
-{
-public:
-    Matrix(int rows, int cols);
-    Matrix(const Matrix& other);
-    ~Matrix();
-
-    static Matrix Identity(int size);
-    static Matrix OuterProduct(const Vector& u, const Vector& v);
-
-    Matrix& operator=(const Matrix& other);
-    Matrix operator+(const Matrix& other) const;
-    Matrix operator-(const Matrix& other) const;
-    Matrix operator*(const Matrix& other) const;
-    Vector operator*(const Vector& v) const;
-
-    int Rows() const;
-    int Cols() const;
+    void OnEntityCreated(CBaseEntity* pEntity, const char* classname);
+    void OnEntityDestroyed(CBaseEntity* pEntity);
 
 private:
-    int m_rows;
-    int m_cols;
-    float* m_data;
+    bool SetupGameInterfaces(char* error, size_t maxlength);
+    bool LoadConfigurations(char* error, size_t maxlength);
+    void InitializeDataStructures();
+    static void OnStartTouch(CBaseEntity* pOther);
 };
 
-// CBaseEntity class
-class CBaseEntity
+class CBrachistochroneOptimizer
 {
 public:
-    CBaseEntity();
-    virtual ~CBaseEntity();
-
-    void SetKeyValues(KeyValues* kvData);
-    KeyValues* GetKeyValues() const;
-    void SetAbsAngles(const QAngle& angles);
-    QAngle GetAbsAngles() const;
-    Vector GetAbsOrigin() const;
-    void SetAbsOrigin(const Vector& origin);
-    Vector GetForwardVector() const;
-    Vector GetUpVector() const;
-    float GetValueForKey(const char* key) const;
+    CBrachistochroneOptimizer(const Ramp& ramp, CBaseEntity* player);
+    std::vector<Vector> Optimize(const Vector& startPos, const Vector& startVel, float tickInterval);
+    void Update(const Vector& playerPos, const Vector& playerVel, const Vector& playerAccel, float tickInterval);
+    bool IsPathValid(const Vector& point) const;
+    const Ramp& GetRamp() const;
+    const std::vector<Vector>& GetPath() const;
+    CBaseEntity* GetPlayer() const;
 
 private:
-    KeyValues* m_KeyValues;
-    QAngle m_AbsAngles;
-    Vector m_AbsOrigin;
+    void PerformFlyCollisionResolution(trace_t& pm, Vector& move);
+
+    Ramp m_Ramp;
+    CBaseEntity* m_Player;
+    std::vector<Vector> m_OptimizedPath;
+    float m_AirAccelerate;
+    float m_Gravity;
+    PlayerMovementTracking m_PlayerTracker;
 };
 
-// PlayerMovementTracking class
 class PlayerMovementTracking
 {
 public:
     PlayerMovementTracking(float maxDeviationDistance, size_t bufferCapacity = 1000);
-
     void UpdatePlayerState(const Vector& position, const Vector& velocity, const Vector& acceleration, float tickInterval);
     Vector EstimatePlayerPosition(float timeStep) const;
     bool HasDeviatedFromPath(const std::vector<Vector>& path) const;
@@ -126,19 +75,17 @@ public:
 
 private:
     float m_MaxDeviationDistance;
-    std::deque<Vector> m_Positions;
-    std::deque<Vector> m_Velocities;
-    std::deque<Vector> m_Accelerations;
-    std::deque<float> m_TimeIntervals;
+    CUtlVector<Vector> m_Positions;
+    CUtlVector<Vector> m_Velocities;
+    CUtlVector<Vector> m_Accelerations;
+    CUtlVector<float> m_TimeIntervals;
 };
 
-// BVHNode class
 class BVHNode
 {
 public:
     BVHNode(const Vector& min, const Vector& max);
     ~BVHNode();
-
     void Insert(Ramp* ramp);
     std::vector<Ramp*> Query(const Vector& point) const;
 
@@ -150,55 +97,110 @@ private:
     std::vector<Ramp*> objects;
 };
 
-// CBrachistochroneOptimizer class
-class CBrachistochroneOptimizer
+class Matrix
 {
 public:
-    CBrachistochroneOptimizer(const Ramp& ramp, CBaseEntity* player);
-
-    std::vector<Vector> Optimize(const Vector& startPos, const Vector& startVel, float tickInterval);
-    void Update(const Vector& playerPos, const Vector& playerVel, const Vector& playerAccel, float tickInterval);
-    bool IsPathValid(const Vector& point) const;
-
-    const Ramp& GetRamp() const;
-    const std::vector<Vector>& GetPath() const;
-    CBaseEntity* GetPlayer() const;
-
-    std::vector<Vector> m_OptimizedPath;
+    Matrix(int rows = 3, int cols = 3);
+    Matrix(const Matrix& other);
+    ~Matrix();
+    static Matrix Identity(int size);
+    static Matrix OuterProduct(const Vector& u, const Vector& v);
+    Matrix& operator=(const Matrix& other);
+    Matrix operator+(const Matrix& other) const;
+    Matrix operator-(const Matrix& other) const;
+    Matrix operator*(const Matrix& other) const;
+    Vector operator*(const Vector& v) const;
+    int Rows() const;
+    int Cols() const;
 
 private:
-    Ramp m_Ramp;
-    CBaseEntity* m_Player;
-    PlayerMovementTracking m_PlayerTracker;
-    float m_AirAccelerate;
-    float m_Gravity;
+    int m_rows;
+    int m_cols;
+    float* m_data;
 };
 
-// SurfRampOptimizer class
-class SurfRampOptimizer : public SDKExtension
+class Vector
 {
 public:
-    virtual bool SDK_OnLoad(char* error, size_t maxlength, bool late) override;
-    virtual void SDK_OnUnload() override;
-    virtual void SDK_OnAllLoaded() override;
-    virtual bool QueryRunning(char* error, size_t maxlength) override;
-    virtual void OnEntityCreated(CBaseEntity* pEntity, const char* classname) override;
-    virtual void OnEntityDestroyed(CBaseEntity* pEntity) override;
+    Vector();
+    Vector(float x, float y, float z);
+    Vector(float x, float y, float z, float v, float w, float u);
+    float LengthSqr() const;
+    float Length() const;
+    Vector Normalized() const;
+    Vector Cross(const Vector& other) const;
+    float Dot(const Vector& other) const;
+    Vector operator+(const Vector& other) const;
+    Vector operator-(const Vector& other) const;
+    Vector operator*(float scalar) const;
+    Vector operator/(float scalar) const;
+    bool operator==(const Vector& other) const;
+    bool operator!=(const Vector& other) const;
 
-    static void OnStartTouch(CBaseEntity* pOther);
-    bool SDK_OnMetamodLoad(ISmmAPI* ismm, char* error, size_t maxlen, bool late);
-
-private:
-    bool SetupGameInterfaces(char* error, size_t maxlength);
-    bool LoadConfigurations(char* error, size_t maxlength);
-    void InitializeDataStructures();
+    float x, y, z;
+    float v, w, u;
 };
 
-// Global variables
-extern SurfRampOptimizer g_SurfRampOptimizer;
-extern std::vector<std::unique_ptr<CBrachistochroneOptimizer>> g_SurfRamps;
-extern BVHNode* g_BVHRoot;
-extern std::unordered_map<CBaseEntity*, Ramp> g_RampCache;
+class Ramp
+{
+public:
+    bool operator==(const Ramp& other) const;
 
-// Utility functions
+    Vector startPoint;
+    Vector endPoint;
+    Vector normal;
+    float length;
+    float width;
+    Vector minExtents;
+    Vector maxExtents;
+};
+
+namespace CollisionDetection
+{
+    void Initialize();
+    void Shutdown();
+    void InsertRamp(Ramp* ramp);
+    bool Intersects(const Vector& point, const Ramp& ramp);
+    bool GJKIntersection(const Ramp& ramp1, const Ramp& ramp2);
+    Vector SupportPoint(const Ramp& ramp1, const Ramp& ramp2, const Vector& direction);
+    Vector FarthestPointInDirection(const Ramp& ramp, const Vector& direction);
+    bool UpdateSimplex(Vector* simplex, Vector& direction, int index);
+}
+
+namespace NumericalMethods
+{
+    Vector SolveBrachistochrone(const Vector& startPos, const Vector& endPos, float time, float gravity, float airAccelerate);
+    Vector SolveBrachistochroneAdaptive(const Vector& startPos, const Vector& endPos, float time, float gravity, float airAccelerate, float errorTolerance);
+    float CalculateBrachistochronePathTime(const Vector& startPos, const Vector& endPos, float gravity);
+    Vector CalculateGradient(const Vector& point, const Ramp& ramp, float gravity, float airAccelerate);
+    float GaussianQuadrature(float a, float b, int n, float (*f)(float, void*), void* params);
+    Vector DormandPrince54(float t0, float tf, const Vector& y0, float (*f)(float, Vector, void*), void* params, float& h);
+}
+
+namespace MathUtils
+{
+    Vector SIMDCrossProduct(const Vector& a, const Vector& b);
+    float SIMDDotProduct(const Vector& a, const Vector& b);
+    float SIMDLength(const Vector& v);
+    Vector SIMDNormalized(const Vector& v);
+    void ClipVelocity(Vector& in, Vector& normal, Vector& out, float overbounce);
+}
+
+namespace Optimization
+{
+    std::vector<Vector> OptimizePath(const std::vector<Vector>& path, const Ramp& ramp, float gravity, float airAccelerate);
+    std::vector<Vector> OptimizePathConjugateGradient(const std::vector<Vector>& path, const Ramp& ramp, float gravity, float airAccelerate);
+    std::vector<Vector> OptimizePathBFGS(const std::vector<Vector>& path, const Ramp& ramp, float gravity, float airAccelerate);
+    bool SatisfiesWolfeConditions(const Vector& point, const Vector& direction, float alpha, const Ramp& ramp, float gravity, float airAccelerate, float c1, float c2);
+    std::pair<float, bool> LineSearch(const Vector& point, const Vector& direction, const Ramp& ramp, float gravity, float airAccelerate);
+    float CalculatePathCost(const Vector& point, const Ramp& ramp, float gravity, float airAccelerate);
+    Vector GetClosestPointOnRamp(const Vector& point, const Ramp& ramp);
+    void UpdateInverseHessian(Matrix& inverseHessian, const Vector& direction, const Vector& gradientDifference);
+}
+
+namespace Pathfinding
+{
+    std::optional<std::vector<Vector>> Pathfinding(const Vector& start, const Vector& goal, const std::vector<Ramp>& ramps);
+}
+
 Ramp GetRampFromEntity(CBaseEntity* pEntity);
